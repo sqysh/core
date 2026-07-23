b1fbbd1773821f21ca2ee06d01b28fef4fd6be71
Gregory Row <rowgregory@gmail.com>
2026-07-23 15:15:08 -0400

    fix:add force-dynamic to public attendance page; add UserEmail with relation to usesr

--- app/(public)/attendance/page.tsx
+++ app/(public)/attendance/page.tsx
@@ -2,6 +2,8 @@ import { getUsers } from '@/app/lib/actions/user/getUsers'
 import AttendanceClient from './AttendanceClient'
 import { getTodayAttendance } from '@/app/lib/actions/meeting/getTodayAttendance'
 
+export const dynamic = 'force-dynamic'
+
 export default async function AttendancePage() {
   const result = await getUsers()
   const attendees = await getTodayAttendance()
